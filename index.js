f2a9231b919f2acfe8fd4462e91faadb190a742f
SPCElijah555 <37822208+SPCElijah555@users.noreply.github.com>
2018-08-18 00:17:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -48,7 +48,7 @@ bot.on("ready", async () => {
 
   console.log(`${bot.user.username} is online on ${bot.guilds.size} servers!`);
 
-  bot.user.setActivity("the SAPW CAD be made", {type: "WATCHING"});
+  bot.user.setActivity("The SAPW Roleplay ", {type: "WATCHING"});
 
    await client.change_presence(game=discord.Game(name='something goes here'));
 
